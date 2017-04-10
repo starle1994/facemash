3a67dfad1911da1bac72118a886a98161ff3e766
le star <le star>
2017-04-10 21:38:29 +0700

    master

--- app/Http/Controllers/Admin/StatisticalController.php
+++ app/Http/Controllers/Admin/StatisticalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Statistical;
+use DateTime;
 
 class StatisticalController extends Controller {
 
@@ -19,29 +20,38 @@ class StatisticalController extends Controller {
 		$day = (int)date('d');
             $month = (int)date('m');
             $year = (int)date('Y');
-
+            $viewClick =0;
+            $percentClick = 0;
+            $timeAvg = 0;
+            $left =0;
+            $right=0;
+            $totalClick=0;
             $total = (int)(Statistical::sum('views'));
+            if ($total !=0) {
+                $viewClick = (int)(Statistical::sum('viewClick'));
 
-            $viewClick = (int)(Statistical::sum('viewClick'));
+                $percentClick = ($viewClick/$total)*100;
 
-            $percentClick = ($viewClick/$total)*100;
+                $percentClick = round($percentClick);
 
-            $percentClick = round($percentClick);
+                $time = (double)(Statistical::sum('timespent'));
 
-            $time = (double)(Statistical::sum('timespent'));
+                $timeAvg = round(($time/(double)$total),2);
 
-            $timeAvg = round(($time/(double)$total),2);
+                $left = (int)(Statistical::sum('numberleft'));
+                $right  = (int)(Statistical::sum('numberright'));
 
-            $left = (int)(Statistical::sum('numberleft'));
-            $right  = (int)(Statistical::sum('numberright'));
+                $totalClick = $left + $right;
+                if ($totalClick !=0) {
+                    $left = ($left/$totalClick)*100;
+                }
+                
 
-            $totalClick = $left + $right;
+                $left = round($left);
 
-            $left = ($left/$totalClick)*100;
-
-            $left = round($left);
-
-            $right = 100 - $left;
+                $right = 100 - $left;
+            }
+            
 
 
             $statistical = Statistical::where('day',$day)->where('month',$month)->where('year',$year)->first();
@@ -49,10 +59,33 @@ class StatisticalController extends Controller {
                 $ondayleft = Statistical::where('day',$day)->where('month',$month)->where('year',$year)->sum('numberleft');
                 $ondayright = Statistical::where('day',$day)->where('month',$month)->where('year',$year)->sum('numberright');
                 $onday = $ondayleft + $ondayright;
+
             }else{
                 $onday = 0;
             }
-            return view('admin.statistical.statistical', compact('totalClick','onday','left','right','timeAvg','percentClick'));
+            $now = new DateTime();
+            $prevDateTime = $now->modify("last day of this month");
+            $days = $prevDateTime->format('d');
+            $month = $prevDateTime->format('m');
+
+            $statistical = Statistical::where('month',$month)->where('year',$year)->get();
+            $date =[];
+            $aaa = [];
+            
+            for ($i=1; $i <= $days ; $i++) { 
+                $date[$i] =0;
+                $aaa[$i] = $i.'/'.$month;
+            }
+           
+            foreach ($date as $key => $value) {
+                foreach ($statistical as $sta) {
+                    if($key == $sta->day){
+                        $date[$key]= $sta->numberleft  + $sta->numberright;
+                    }
+                }
+            }
+          
+            return view('admin.statistical.statistical', compact('totalClick','onday','left','right','timeAvg','percentClick','date','aaa'));
 	}
 
 }
--- resources/views/admin/statistical/statistical.blade.php
+++ resources/views/admin/statistical/statistical.blade.php
@@ -8,7 +8,19 @@
 	.bg{
 		background-color: #fff; padding: 20px
 	}
+	.container {
+  width: 80%;
+  margin: 20px auto;
+}
+
+.p {
+  text-align: center;
+  font-size: 14px;
+  padding-top: 140px;
+}
 </style>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
 <div class="row">
 	<div class="col-sm-4 bg" >
 		<p>Tổng Số lần truy Cập </p>
@@ -19,11 +31,73 @@
 		<p>Số Lần truy cập trong ngày </p>
 		<p class="text-right number" ><b> {{$onday}} </b></p>
 	</div>
-</div>
 	
-	 
-	 <p>Phần trăm trái : <b> {{$left}} % </b></p>
-	 <p>Phần trăm phải : <b> {{$right}} % </b> </p>
-	 <p>Số Thời gian trung bình của mỗi lần truy cập : <b> {{$timeAvg}} minute </b> </p>
-	 <p>Số phần trăm người Click : <b> {{$percentClick}} % </b></p>
+</div>
+<br>
+<div class="row">
+	<div class="col-sm-4 bg" >
+		<p>Left</p>
+		<p class="text-right number" > <b> {{$left}} %</b></p>
+	</div>
+	<div class="col-sm-1"></div>
+	<div class="col-sm-4 bg">
+		<p>Right </p>
+		<p class="text-right number" ><b> {{$right}} %  </b></p>
+	</div>
+</div>
+<br>
+<div class="row">
+	<div class="col-sm-12" style="background-color: #fff">
+	  <div>
+	    <canvas id="canvas"></canvas>
+	  </div>
+	</div>
+</div>
+<br>
+<div style="clear: both;"></div>
+<div class="row">
+	<div class="col-sm-4 bg" >
+		<p class="text-center number" > <b> {{$timeAvg}} minute %</b></p>
+	</div>
+	<div class="col-sm-1"></div>
+	<div class="col-sm-4 bg">
+		<p class="text-center number" ><b>{{$percentClick}} % </b></p>
+	</div>
+</div>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'></script>
+<script type="text/javascript">
+	var arrName = <?php echo json_encode($date)?>;
+
+	var aaa = <?php echo json_encode($aaa)?>;
+console.log(aaa,arrName);
+var lineChartData = {
+    labels: [aaa[1], aaa[2], aaa[3], aaa[4], aaa[5], aaa[6], aaa[7],aaa[8], aaa[9], aaa[10], aaa[11], aaa[12], aaa[13], aaa[14], aaa[15],aaa[16], aaa[17], aaa[18], aaa[19], aaa[20], aaa[21], aaa[22],aaa[23], aaa[24], aaa[25], aaa[26], aaa[27], aaa[28], aaa[29], aaa[30]],
+    datasets: [{
+        fillColor: "rgba(220,220,220,0)",
+        strokeColor: "rgba(220,180,0,1)",
+        pointColor: "rgba(220,180,0,1)",
+        data: arrName
+    }]
+
+}
+
+Chart.defaults.global.animationSteps = 50;
+Chart.defaults.global.tooltipYPadding = 16;
+Chart.defaults.global.tooltipCornerRadius = 0;
+Chart.defaults.global.tooltipTitleFontStyle = "normal";
+Chart.defaults.global.tooltipFillColor = "rgba(0,160,0,0.8)";
+Chart.defaults.global.animationEasing = "easeOutBounce";
+Chart.defaults.global.responsive = true;
+Chart.defaults.global.scaleLineColor = "black";
+Chart.defaults.global.scaleFontSize = 16;
+
+var ctx = document.getElementById("canvas").getContext("2d");
+var LineChartDemo = new Chart(ctx).Line(lineChartData, {
+    pointDotRadius: 10,
+    bezierCurve: false,
+    scaleShowVerticalLines: false,
+    scaleGridLineColor: "black"
+});
+</script>
 @endsection
