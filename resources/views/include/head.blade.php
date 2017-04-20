a314216b74589446c6dfe8ff6d56db7c6b83fdaa
saole <lethisao1894@gmail.com>
2017-04-20 10:53:05 +0700

    seo

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -4,7 +4,10 @@
 <meta name="description" content="メタディスクリプション
 名古屋のすべてに順位をつけよう。２つの画像をクリックするだけでNo.1が決まります。名古屋めし、ラーメン、スイーツ、観光地などの定番ランキングから、クラブ、ホスト、キャバクラなどのディープなランキングまで、あらゆるジャンルに対応。" >
 <meta name="keywords" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス" > 
-<title>XOXO</title>
+<meta name="author" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
+<meta name="copyright" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
+<link rel="canonical" href="route('index')">
+<title>名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス</title>
 <link href='{!! url('css/bootstrap.min.css') !!}' rel='stylesheet' type='text/css' />
 
 <link rel="stylesheet" href="{!! url('css/style.css') !!}"/>
