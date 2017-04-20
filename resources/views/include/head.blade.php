80d8482fae7583c29372d9e8e4429a980e459774
le star <le star>
2017-04-20 20:18:29 +0700

    a

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,9 +1,10 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="google-site-verification" content="Ds3c45ZbOW9e2xVVC7_JRZ2lBOOhFpNtbi7jEJfNvps" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="メタディスクリプション
 名古屋のすべてに順位をつけよう。２つの画像をクリックするだけでNo.1が決まります。名古屋めし、ラーメン、スイーツ、観光地などの定番ランキングから、クラブ、ホスト、キャバクラなどのディープなランキングまで、あらゆるジャンルに対応。" >
-<meta name="keywords" content="XOXO(ハグ&キス)" > 
+<meta name="keywords" content="ハグ&キス"> 
 <meta name="author" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
 <meta name="copyright" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
 <link rel="canonical" href="route('index')">
