9fa8e2ddb60cf8e219d73374e853c334c5f934d2
le star <le star>
2017-04-20 12:52:50 +0700

    seo

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -3,11 +3,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="メタディスクリプション
 名古屋のすべてに順位をつけよう。２つの画像をクリックするだけでNo.1が決まります。名古屋めし、ラーメン、スイーツ、観光地などの定番ランキングから、クラブ、ホスト、キャバクラなどのディープなランキングまで、あらゆるジャンルに対応。" >
-<meta name="keywords" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス" > 
+<meta name="keywords" content="xoxo-好きな方にプッシュ！ " > 
 <meta name="author" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
 <meta name="copyright" content="名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス">
 <link rel="canonical" href="route('index')">
-<title>名古屋の人気ランキングはあなたが決める-XOXO(ハグ&キス)ハグキス</title>
+<title> xoxo-好きな方にプッシュ！</title>
 <link href='{!! url('css/bootstrap.min.css') !!}' rel='stylesheet' type='text/css' />
 
 <link rel="stylesheet" href="{!! url('css/style.css') !!}"/>
