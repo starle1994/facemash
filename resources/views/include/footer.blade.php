4171893ce7f79acbfb9b5dc2cbb0b99f1a464ffc
saole <lethisao1894@gmail.com>
2017-04-21 14:37:06 +0700

    s

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,2 +1,5 @@
-<b><a href="{!! route('index') !!}">XOXO(ハグ&キス)</a></b>
-<b><a href="{!! route('index') !!}">好きな方にプッシュ!</a></b>
+<b><a href="{!! route('index') !!}">XOXO(ハグ&キス)</a>-</b>
+<b><a href="{!! route('index') !!}">好きな方にプッシュ!</a>-</b>
+<b><a href="https://www.youtube.com/watch?v=O6oNd7MY_HQ">xoxo プロモーション スイーツin名古屋 編 </a>-</b>
+<b><a href="https://www.youtube.com/watch?v=gFdIyk__GUo">xoxoプロモーション ホスト編</a>-</b>
+<b><a href="https://www.youtube.com/watch?v=jF1DhOtraU4">xoxoプロモーション</a>-</b>
