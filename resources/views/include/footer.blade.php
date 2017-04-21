437c82e111d4ff7e382773715e925a0b34c8ad23
saole <lethisao1894@gmail.com>
2017-04-21 14:56:05 +0700

    seo

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,5 +1,53 @@
 <b><a href="{!! route('index') !!}">XOXO(ハグ&キス)</a>-</b>
 <b><a href="{!! route('index') !!}">好きな方にプッシュ!</a>-</b>
-<b><a href="https://www.youtube.com/watch?v=O6oNd7MY_HQ">xoxo プロモーション スイーツin名古屋 編 </a>-</b>
-<b><a href="https://www.youtube.com/watch?v=gFdIyk__GUo">xoxoプロモーション ホスト編</a>-</b>
-<b><a href="https://www.youtube.com/watch?v=jF1DhOtraU4">xoxoプロモーション</a>-</b>
+<b><a data-toggle="modal" data-target="#target_1">xoxo プロモーション スイーツin名古屋 編 </a>-</b>
+<b><a data-toggle="modal" data-target="#target_2">xoxoプロモーション ホスト編</a>-</b>
+<b><a data-toggle="modal" data-target="#target_3">xoxoプロモーション</a>-</b>
+<div id="target_1" class="modal fade" data-backdrop="static"  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">xoxo プロモーション スイーツin名古屋 編</h4>
+        </div>
+        <div class="embed-responsive embed-responsive-16by9" class="modal-body" >
+          <iframe  src="https://www.youtube.com/embed/O6oNd7MY_HQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="modal-footer text-center">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+</div> 
+<div id="target_2" class="modal fade" data-backdrop="static"  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">xoxoプロモーション ホスト編</h4>
+        </div>
+        <div class="embed-responsive embed-responsive-16by9" class="modal-body" >
+          <iframe  src="https://www.youtube.com/embed/gFdIyk__GUo" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="modal-footer text-center">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+</div> 
+<div id="target_3" class="modal fade" data-backdrop="static"  >
+	<div class="modal-dialog">
+	  <div class="modal-content">
+	    <div class="modal-header">
+	      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	      <h4 class="modal-title">xoxoプロモーション</h4>
+	    </div>
+	    <div class="embed-responsive embed-responsive-16by9" class="modal-body" >
+	      <iframe  src="https://www.youtube.com/embed/jF1DhOtraU4" frameborder="0" allowfullscreen></iframe>
+	    </div>
+	    <div class="modal-footer text-center">
+	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	    </div>
+	  </div>
+	</div>
+</div> 
