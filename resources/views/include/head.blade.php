9b1e63fe7ea7c97f14fa0f9fd4ae8649397745d8
saole <lethisao1894@gmail.com>
2017-05-24 14:27:07 +0700

    quang cao

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,3 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8855221633077463",
+    enable_page_level_ads: true
+  });
+</script>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="google-site-verification" content="Ds3c45ZbOW9e2xVVC7_JRZ2lBOOhFpNtbi7jEJfNvps" />
